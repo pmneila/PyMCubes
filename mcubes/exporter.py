b57f06e4858ed364e2f00680ea27f5faa4d76dce
Jordan Matelsky <j6k4m8@gmail.com>
2016-01-30 23:19:52 -0500

    add export to obj

--- mcubes/exporter.py
+++ mcubes/exporter.py
@@ -11,10 +11,10 @@ def export_obj(vertices, triangles, filename):
         lines = []
         
         for v in vertices:
-            lines.append("v {}".format(" ".join([str(p) for p in v]))
+            lines.append("v {}".format(" ".join([str(p) for p in v])))
             
         for f in triangles:
-            lines.append("f {}".format(" ".join([str(p) for p in v]))
+            lines.append("f {}".format(" ".join([str(p) for p in v])))
         
         fh.write("\n".join(lines))
 
