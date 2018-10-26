8c80ac075dc5386a9dc17aff71685f6b8d33cdf3
Pablo Márquez Neila <pmarquezneila@gmail.com>
2018-10-26 16:12:07 +0200

    Fixed support for Python2.7 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class lazy_cythonize(list):
     """
     
     def __init__(self, callback):
-        super().__init__()
+        super(lazy_cythonize, self).__init__()
         self._list, self.callback = None, callback
     
     def c_list(self):
