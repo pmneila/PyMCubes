e57047ee6869b66e6667bc1bbae43e127993e1dd
Pablo Márquez Neila <pmarquezneila@gmail.com>
2020-03-28 17:38:37 +0100

    Add tests with invalid parameters for smoothing.py

--- test_smoothing.py
+++ test_smoothing.py
@@ -1,5 +1,7 @@
 
 
+import pytest
+
 import numpy as np
 
 import mcubes
@@ -31,20 +33,58 @@ def test_sphere():
     assert np.all(np.abs(smoothed_levelset - levelset) < 1)
 
 
-def test_large_sphere():
+def test_gaussian_smoothing():
 
-    # Create sphere with radius 200 centered at (300, 300, 300)
-    x, y, z = np.mgrid[:600, :600, :600]
-    levelset = np.sqrt((x - 300)**2 + (y - 300)**2 + (z - 300)**2) - 200
+    # Create sphere with radius 25 centered at (50, 50, 50)
+    x, y, z = np.mgrid[:100, :100, :100]
+    levelset = np.sqrt((x - 50)**2 + (y - 50)**2 + (z - 50)**2) - 25
 
     binary_levelset = levelset > 0
-    smoothed_levelset = mcubes.smooth(binary_levelset)
+    smoothed_levelset = mcubes.smooth(
+        binary_levelset,
+        method='gaussian',
+        sigma=3
+    )
 
     vertices, _ = mcubes.marching_cubes(smoothed_levelset, 0.0)
 
-    # Check all vertices have same distance to (300, 300, 300)
-    dist = np.sqrt(np.sum((vertices - [300, 300, 300])**2, axis=1))
-    assert dist.min() > 199.5 and dist.max() < 200.5
+    # Check all vertices have same distance to (50, 50, 50)
+    dist = np.sqrt(np.sum((vertices - [50, 50, 50])**2, axis=1))
+    assert dist.min() > 24 and dist.max() < 25
+
+
+def test_wrong_ndim():
+
+    binary_levelset = np.random.uniform(size=(10)) < 0.5
+
+    with pytest.raises(ValueError):
+        mcubes.smooth(
+            binary_levelset,
+            method='constrained',
+            max_iters=500,
+            rel_tol=1e-4
+        )
+
+    binary_levelset = np.random.uniform(size=(10, 10, 10, 10)) < 0.5
+
+    with pytest.raises(ValueError):
+        mcubes.smooth(
+            binary_levelset,
+            method='constrained',
+            max_iters=500,
+            rel_tol=1e-4
+        )
+
+
+def test_wrong_method():
+
+    with pytest.raises(ValueError):
+        mcubes.smooth(
+            np.zeros((10, 10), dtype=np.bool_),
+            method='wrong',
+            max_iters=500,
+            rel_tol=1e-4
+        )
 
 
 def test_circle():
@@ -62,8 +102,8 @@ def test_circle():
     assert np.all(np.abs(smoothed_levelset - levelset) < 1)
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 #     # logging.basicConfig(level=logging.DEBUG)
 #     test_circle()
 #     test_sphere()
-    test_large_sphere()
+#     test_large_sphere()
