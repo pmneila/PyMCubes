e2ce978eff51635d5b0c8f5d9df129969e3ff7f3
Pablo Márquez Neila <pmarquezneila@gmail.com>
2020-03-28 17:29:26 +0100

    Add test with large sphere

--- test_smoothing.py
+++ test_smoothing.py
@@ -31,13 +31,28 @@ def test_sphere():
     assert np.all(np.abs(smoothed_levelset - levelset) < 1)
 
 
+def test_large_sphere():
+
+    # Create sphere with radius 200 centered at (300, 300, 300)
+    x, y, z = np.mgrid[:600, :600, :600]
+    levelset = np.sqrt((x - 300)**2 + (y - 300)**2 + (z - 300)**2) - 200
+
+    binary_levelset = levelset > 0
+    smoothed_levelset = mcubes.smooth(binary_levelset)
+
+    vertices, _ = mcubes.marching_cubes(smoothed_levelset, 0.0)
+
+    # Check all vertices have same distance to (300, 300, 300)
+    dist = np.sqrt(np.sum((vertices - [300, 300, 300])**2, axis=1))
+    assert dist.min() > 199.5 and dist.max() < 200.5
+
+
 def test_circle():
 
     x, y = np.mgrid[:100, :100]
     levelset = np.sqrt((x - 50)**2 + (y - 50)**2) - 25
     binary_levelset = levelset > 0
 
-    # u1 = mcubes.signed_distance_function(u)
     smoothed_levelset = mcubes.smooth(
         binary_levelset,
         max_iters=500,
@@ -47,7 +62,8 @@ def test_circle():
     assert np.all(np.abs(smoothed_levelset - levelset) < 1)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 #     # logging.basicConfig(level=logging.DEBUG)
 #     test_circle()
 #     test_sphere()
+    test_large_sphere()
